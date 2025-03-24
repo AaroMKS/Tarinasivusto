0069e43e1fbafaa17dec98f8c7e6067594fcac96
Aaro Savolainen <aaro.savolainen@helsinki.fi>
2025-03-24 14:59:41 +0200

    Improve user acces checks

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ import items
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
+def require_login():
+    if "user_id" not in session:
+        abort(403)
+
 @app.route("/")
 def index():
     all_items=items.get_items()
@@ -34,9 +38,11 @@ def show_item(item_id):
     
 @app.route("/new_item")
 def new_item():
+    require_login()
     return render_template("new_item.html")
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
+    require_login()
     item=items.get_item(item_id)
     if not item:
          abort(404)
@@ -46,6 +52,7 @@ def edit_item(item_id):
 
 @app.route("/remove_item/<int:item_id>", methods=["GET","POST"])
 def remove_item(item_id):
+    require_login()
     item=items.get_item(item_id)
     if not item:
          abort(404)
@@ -62,6 +69,7 @@ def remove_item(item_id):
 
 @app.route("/update_item", methods=["POST"])
 def update_item():
+    require_login()
     item_id= request.form["item_id"]
     item=items.get_item(item_id)
     if not item:
@@ -77,6 +85,8 @@ def update_item():
 
 @app.route("/create_item", methods=["POST"])
 def create_item():
+    require_login()
+
     title=request.form["title"]
     description=request.form["description"]
     story=request.form["story"]
@@ -125,6 +135,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    del session["user_id"]
-    del session["username"]
+    if "user_id" in session:
+        del session["user_id"]
+        del session["username"]
     return redirect("/")
