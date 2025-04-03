e0d7054d71c8aefe3bdee5aec79ddd3df12f28d6
Aaro Savolainen <aaro.savolainen@helsinki.fi>
2025-04-03 19:32:35 +0300

    Improve navigation

--- items.py
+++ items.py
@@ -65,3 +65,19 @@ def find_items(query):
     sql= """SELECT id, title FROM items WHERE title LIKE ? OR description LIKE ? ORDER BY id DESC"""
     like = "%" + query + "%"
     return db.query(sql, [like, like])
+
+def get_images(item_id):
+    sql="SELECT id FROM images WHERE item_id=?"
+    result=db.query(sql, [item_id])
+    return result 
+
+def add_image(item_id, image):
+    sql="INSERT INTO images (item_id, image) VALUES (?, ?)"
+    
+    db.execute(sql, [item_id, image])
+
+def get_image(image_id):
+    sql="SELECT id, image FROM images WHERE id=?"
+    result=db.query(sql, [image_id])
+
+    return result[0][0] if result else None
