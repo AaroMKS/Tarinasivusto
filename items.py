ddafababb6e30c3da37e5f565b3e1823119142df
Aaro Savolainen <aaro.savolainen@helsinki.fi>
2025-04-05 20:50:10 +0300

    Fix remove item

--- items.py
+++ items.py
@@ -61,6 +61,10 @@ def update_item(item_id, title, description, story, classes):
 def remove_item(item_id):
     sql= "DELETE FROM item_classes WHERE item_id = ?"
     db.execute(sql, [item_id])
+    sql= "DELETE FROM reviews WHERE item_id = ?"
+    db.execute(sql, [item_id])
+    sql= "DELETE FROM images WHERE item_id = ?"
+    db.execute(sql, [item_id])
     sql= "DELETE FROM items WHERE id = ?"
     db.execute(sql, [item_id])
 
